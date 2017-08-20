0725705c42c6d9822811f1c466fd98b4b3c5c78c
vitaz-0 <vitezslav.zak@seznam.cz>
2017-08-20 22:21:20 +0200

    Zobrazovani regionu

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -13,6 +13,7 @@ export default class MapScreen extends Component {
   constructor(props) {
     super(props)
     this.dataRef = firebaseApp.database().ref('/trails/trailGeo/geo').orderByChild("trailID").equalTo(this.props.trailID);
+    this.mapRef = null;
 
     this.state = {
       geoPoints: {
@@ -21,118 +22,112 @@ export default class MapScreen extends Component {
           points: [],
         },
       },
-      region: {
-        //latitude: 50.551747000000006,
-        //latitudeDelta:0.04510200000000197,
-        //longitude:16.0160125,
-        //longitudeDelta: 0.07857899999999794
-      }
+      region: {}
     }
     this.mapRef = null;
+  }
+
+  _hasRegion(){
+    if(this.state.geoPoints.boundaries.hasOwnProperty('sw_lat')
+      && this.state.geoPoints.boundaries.hasOwnProperty('sw_lng')
+      && this.state.geoPoints.boundaries.hasOwnProperty('ne_lat')
+      && this.state.geoPoints.boundaries.hasOwnProperty('ne_lng')
+    ){
+      return true;
+    } else {
+      console.log("WARNING: Trasa nema region. Using default. ");
+      return false;
+    }
   }
 
   listenForCoords(dataRef) {
     dataRef.on('value', (dataSnapshot) => {
       var coords = [];
+
       dataSnapshot.forEach((child) => {
         coords.push(child.val());
       });
-      console.log("COORDS");
-      console.log(coords);
+
+      if(!coords[0].hasOwnProperty('geoPoints') || !coords[0].geoPoints.hasOwnProperty('points')){
+        console.log("WARNING: Trasa nema markery. ");
+        coords[0].geoPoints.points = [];
+      }
+
       this.setState({
         geoPoints: coords[0]
       });
-      console.log("LISTEN FOR COORDS, GEOPOINTS");
-      console.log(this.state.geoPoints);
+
+      if(this._hasRegion()){
+        this.mapRef.fitToCoordinates(
+          [{latitude: this.state.geoPoints.boundaries.sw_lat, longitude: this.state.geoPoints.boundaries.sw_lng},
+            {latitude: this.state.geoPoints.boundaries.ne_lat, longitude: this.state.geoPoints.boundaries.ne_lng}],
+            { edgePadding: { top: 20, right: 20, bottom: 20, left: 20 }, animated: false }
+        );}
     });
   }
 
-  _setRegion(){
-    /*if(
-      this.state.geoPoints.boundaries.hasOwnProperty('sw_lat')
-      && this.state.geoPoints.boundaries.hasOwnProperty('sw_lng')
-      && this.state.geoPoints.boundaries.hasOwnProperty('ne_lat')
-      && this.state.geoPoints.boundaries.hasOwnProperty('ne_lng')
-    ){
-      var sw_lat = this.state.geoPoints.boundaries.sw_lat;
-      var sw_lng = this.state.geoPoints.boundaries.sw_lng;
-      var ne_lat = this.state.geoPoints.boundaries.ne_lat;
-      var ne_lng = this.state.geoPoints.boundaries.ne_lng;
-    } else {
-      // DEFAULT FOR CZECH REPUBLIC
-      console.log("Using default boundaries");
-      var sw_lng = 12.091389;
-      var sw_lat = 48.5525;
-      var ne_lng = 18.858889;
-      var ne_lat = 51.055556;
-    }
-    */
-
-    var sw_lat = this.state.geoPoints.boundaries.sw_lat;
-    var sw_lng = this.state.geoPoints.boundaries.sw_lng;
-    var ne_lat = this.state.geoPoints.boundaries.ne_lat;
-    var ne_lng = this.state.geoPoints.boundaries.ne_lng;
-
-    this.state.region = {
+  _getInitialRegion(){
+    var sw_lng = 12.091389;
+    var sw_lat = 48.5525;
+    var ne_lng = 18.858889;
+    var ne_lat = 51.055556;
+    var region = {
         latitude:     (ne_lat + sw_lat)/2,
         longitude:    (ne_lng + sw_lng)/2,
         latitudeDelta: ne_lat - sw_lat + SPACE,
         longitudeDelta:ne_lng - sw_lng + SPACE,
     }
-
-    console.log("SET COORDS, REGION");
-    console.log(this.state.region);
-
+    return region;
   }
 
-
   componentDidMount() {
-    console.log("Component will mount....");
+    console.log("Component did mount....");
     this.listenForCoords(this.dataRef);
   }
 
   onRegionChange(region) {
-  this.setState({ region });
-  //console.log("region change");
+    this.setState({ region });
   }
 
   render() {
-    this._setRegion();
-
     return (
       <MapView style={styles.map}
-
+        ref={(ref) => { this.mapRef = ref }}
 
       provider={PROVIDER_GOOGLE}
-      initialRegion={this.state.region}
+      initialRegion={this._getInitialRegion()}
       onRegionChange={this.onRegionChange.bind(this)}
+
       showsMyLocationButton={true}
       showsCompass={true}
       showsScale={true}
       zoomEnabled={true}
       >
-      {this.state.geoPoints.geoPoints.points.map(marker => (
-        <MapView.Marker
-          key={marker.cislo}
-          identifier={"MRK_"+marker.cislo}
-          coordinate={marker.latLng}
-          title={marker.nazev}/>))}
+
+      {
+          this.state.geoPoints.geoPoints.points.map(marker => (
+            <MapView.Marker
+              key={marker.cislo}
+              identifier={"MRK_"+marker.cislo}
+              coordinate={marker.latLng}
+              title={marker.nazev}/>))
+
+      }
       </MapView>
-
-    )}
+    )
   }
+}
 
-  const styles = StyleSheet.create({
-
- map: {
-   position: 'absolute',
-   top: 64,
-   left: 0,
-   right: 0,
-   bottom: 0,
-   borderBottomColor: '#a7a6ab',
-   borderBottomWidth: 0.5,
- },
+const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    top: 64,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderBottomColor: '#a7a6ab',
+    borderBottomWidth: 0.5,
+  },
 });
 
 module.exports = MapScreen
